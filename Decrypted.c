543d825af8a8a94651f4d6a94c095a3268a9b250
Master Hunter <ikrp3610@gmail.com>
2024-08-26 00:26:37 +0300

    Update Decrypted.c

--- Decrypted.c
+++ Decrypted.c
@@ -1,82 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
 
-#define MAX_LENGTH 64
-#define MIN_LENGTH 30
+#define MAX_LENGTH 2048
 
+char decryptChar(const char *input) {
+    int encryptedValue;
+    char randomLetter;
+    char randomNumber;
+    char randomWord[10];
+    
+    sscanf(input, "%d %c %c %s", &encryptedValue, &randomLetter, &randomNumber, randomWord);
 
-int binaryDecode(const char *input) {
-    int value = 0;
-    while (*input) {
-        value = (value << 1) | (*input - '0');
-        input++;
+    int asciiValue = (encryptedValue - 7 + 256) / 3; 
+    return (char)asciiValue;
+}
+
+void decryptText(const char *input, char *output) {
+    char buffer[MAX_LENGTH];
+    const char *ptr = input;
+    int index = 0;
+    char decryptedChar;
+    char encryptedPart[50];
+
+    while (*ptr) {
+        sscanf(ptr, "%s", encryptedPart);
+        ptr += strlen(encryptedPart) + 1;
+        decryptedChar = decryptChar(encryptedPart);
+        buffer[index++] = decryptedChar;
     }
-    return value;
-}
-
-
-int hexDecode(const char *input) {
-    int value;
-    sscanf(input, "%X", &value);
-    return value;
-}
-
-
-int decimalDecode(const char *input) {
-    int value;
-    sscanf(input, "%d", &value);
-    return value;
-}
-
-void customDecrypt(char *input, char *output) {
-    int inputLength = strlen(input);
-    int outputIndex = 0;
-
-    for (int i = 0; i < inputLength;) {
-        char buffer[9] = {0};
-
-        if (strncmp(input + i, "Hunter", 6) == 0) {
-            i += 6; 
-            continue;
-        }
-
-        if (isdigit(input[i])) {
-            if (i + 7 < inputLength && (input[i] == '0' || input[i] == '1')) {
-                strncpy(buffer, input + i, 8);
-                output[outputIndex++] = (char)binaryDecode(buffer);
-                i += 8;
-            } else if (isxdigit(input[i + 1])) { 
-                strncpy(buffer, input + i, 2);
-                output[outputIndex++] = (char)hexDecode(buffer);
-                i += 2;
-            } else {
-                int j = 0;
-                while (isdigit(input[i + j])) {
-                    buffer[j] = input[i + j];
-                    j++;
-                }
-                output[outputIndex++] = (char)decimalDecode(buffer);
-                i += j;
-            }
-        } else if (isalpha(input[i]) || ispunct(input[i])) {
-            i++; 
-        } else {
-            i++; 
-        }
-    }
-
-    output[outputIndex] = '\0';
+    
+    buffer[index] = '\0';
+    strcpy(output, buffer);
 }
 
 int main() {
-    char input[MAX_LENGTH + 1] = "011010000110010101101100Hunter6C6F";
-    char output[MAX_LENGTH + 1];
+    char encryptedText[MAX_LENGTH] = "183 A 4 zebra 193 B 8 moose 155 C 2 quartz 140 D 1 vivid";
+    char decrypted[MAX_LENGTH] = {0};
 
-    customDecrypt(input, output);
-
-    printf("Decrypted: %s\n", output);
+    decryptText(encryptedText, decrypted);
+    printf("Decrypted: %s\n", decrypted);
 
     return 0;
 }
